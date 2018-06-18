d74b6c520af77f794a28942b1b6fd47d7b6e777f
JesseL <JESSE.LOFTUS@GMAIL.COM>
2018-06-18 12:35:43 -0700

    working out when to call URL

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,13 +1,21 @@
-var queryURL = "https://api.giphy.com/v1/gifs/" + option + "?api_key=" + API_key + search;
 let API_key = '0Al2XT5XD35ahPXsk85pGa6yi75wv2ga';
-let option = ''
-let search = '&tag='
+let endpoint = ['trending','search','random'];
+let search = {trending: '&tag=',search: '&q='}; // concatenate strings with input text
+let topic = ['cow', 'outside', 'diabeetus', 'bronchitis'];
+let limit = '&limit=10'; 
+let rating = '&rating=R';
+let queryURL = "https://api.giphy.com/v1/gifs/" + endpoint[1] + "?api_key=" + API_key + search + limit + rating;
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  })
 
-  .then(function(response) {
-
-  });
+$( document ).ready(function() {
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+    
+      .then(function(response) {
+        topic.forEach(function(t) {
+            $('#giphyButtons').append(`<button type="button" class="btn btn-secondary">${t}</button>`)
+        })
+      });
+});

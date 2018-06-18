46c2a8431e553f3a97ca5c769b5be414a7ba08b4
JesseL <JESSE.LOFTUS@GMAIL.COM>
2018-06-18 12:36:49 -0700

    reset HEAD

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,18 +4,25 @@ let search = {trending: '&tag=',search: '&q='}; // concatenate strings with inpu
 let topic = ['cow', 'outside', 'diabeetus', 'bronchitis'];
 let limit = '&limit=10'; 
 let rating = '&rating=R';
-let queryURL = "https://api.giphy.com/v1/gifs/" + endpoint[1] + "?api_key=" + API_key + search + limit + rating;
-
+// let starterURL = "https://api.giphy.com/v1/gifs/" + endpoint[1] + "?api_key=" + API_key + search.search + limit + rating;
 
 $( document ).ready(function() {
+    createTopicButtons();
+    
+    
+    let queryURL = "https://api.giphy.com/v1/gifs/" + endpoint[1] + "?api_key=" + API_key + search.search + limit + rating;
+    
     $.ajax({
         url: queryURL,
         method: "GET"
-      })
+    })
     
-      .then(function(response) {
-        topic.forEach(function(t) {
-            $('#giphyButtons').append(`<button type="button" class="btn btn-secondary">${t}</button>`)
-        })
-      });
+    .then(function(response) {
+    });
 });
+
+function createTopicButtons() {
+    topic.forEach(function(t) {
+        $('#giphyButtons').append(`<button id="btn-${t}" type="button" class="btn btn-secondary">${t}</button>`)
+    })
+};

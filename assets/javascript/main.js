31192482c5de52edd8645d143c912047703d6581
JesseL <35356853+loftusjl@users.noreply.github.com>
2018-06-21 13:24:31 -0700

    changed color scheme to a watermelon theme

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,11 +1,11 @@
 let API_key = '0Al2XT5XD35ahPXsk85pGa6yi75wv2ga';
-let endpoint = ['trending', 'search', 'random'];
+let endpoint = ['trending', 'search'];
 let search = { // concatenate strings with input text
     trending: '&tag=',
     search: '&q='
 };
 let typeSelected = 1; // selects the search type. Search by default
-let topic = ['cow', 'outside', 'diabeetus', 'bronchitis'];
+let topic = ['old greg', 'come at me bro', 'diabeetus', 'bronchitis', 'honey badger'];
 let limit = '&limit=10';
 let rating = '&rating=R';
 
@@ -19,7 +19,27 @@ $(document).ready(function () {
     $('#gifSection').on('click', 'img', function () {
         toggleAnimation($(this));
     })
+    $('#random-gifs').on('click', function(event) {
+        event.preventDefault();
+        let queryURL = "https://api.giphy.com/v1/gifs/" + endpoint[2] + "?api_key=" + API_key + limit + rating;
+        console.log(queryURL)
+        console.log(`Work in progress.....`)
+    
+        $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+    
+            .then(function (response) {
+                console.log(response.data);
 
+                    let gifImg = $(`<img class="" data-animationOn="false" "data-animated="${response.data.images.fixed_width.url}" src="${response.data.images.fixed_width_still.url}">`);
+                    let newGifDiv = $(`<div class="d-flex flex-column text-center my-auto mx-1 randomGif"></div>`)
+                    $('#gifSection').prepend(newGifDiv);
+                    $(`.randomGif`).append(gifImg);
+            });
+    })
+});
 
 // FUNCTIONS
 function outputGifs(clicked) {
@@ -32,9 +52,7 @@ function outputGifs(clicked) {
         })
 
         .then(function (response) {
-            console.log(response);
             console.log(response.data);
-            output = response.data;
 
             // output gifs
             for (i = 0; i < response.data.length; i++) {
@@ -42,20 +60,16 @@ function outputGifs(clicked) {
                 let gifImg = $(`<img class="" data-animationOn="false" data-index="${i} "data-animated="${response.data[i].images.fixed_width.url}" src="${response.data[i].images.fixed_width_still.url}">`);
                 let rating = $(`<p>`).text(`Rating: ${response.data[i].rating}`);
                 let newGifDiv = $(`<div id="gifDIV-${i}"  class="d-flex flex-column text-center my-auto mx-1"></div>`)
-                console.log(response.data[i].images.fixed_width_still.url);
                 $('#gifSection').prepend(newGifDiv);
                 $(`#gifDIV-${i}`).append(gifImg, rating);
             }
-
         });
-
 };
 
 function toggleAnimation(i) {
     dIndex = parseInt(i.attr('data-index'))
     let ani1URL = '';
     let ani2URL = '';
-    console.log(`Toggle Animation`)
     ani1URL = i.attr('data-animated') // animated URL is in data-animated attr
     ani2URL = i.attr('src') // still URL is currently set to img src
     i.attr('data-animated', ani2URL) // set data-animated to off URL for storage
@@ -64,9 +78,9 @@ function toggleAnimation(i) {
 
 function createTopicButtons() {
     topic.forEach(function (t) {
-        $('#giphyButtons').append(`<button id="btn-${t}" type="button" class="btn btn-secondary topic-button">${t}</button>`)
+        $('#giphyButtons').append(`<button id="btn-${t}" type="button" class="btn btn-secondary topic-button font-weight-bold shadow">${t}</button>`)
     })
-    $('#giphyButtons .btn').on('click', function () {
+    $('#giphyButtons .btn').on('click', function () { // listen for topic clicks. Listener is inside createTopicButtons so that when buttons are added to, the listener even still works.
         outputGifs($(this));
     })
 };
@@ -77,8 +91,7 @@ function addButton(s) {
     $('.topic-button').remove();
     createTopicButtons();
 };
-});
-// ADD SEARCH FUNCTION
+
 // $('#add-search').on('click', function() {
 //     typeSelected = parseInt($(this).val());
 //     if (typeSelected === 2) {
